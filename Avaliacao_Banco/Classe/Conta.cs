d4e83d325dccd492f1bf1e2ec7224ea0e01870cb
Lucas Pires <luc_50@outlook.com>
2019-09-18 13:06:13 -0300

    1º Commit - Criado as classe banco, conta, agência, cliente - adicionado atributos e metodos a classe conta.

---
+++ Avaliacao_Banco/Classe/Conta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaliacao_Banco.Classe
+{
+    class Conta
+    {
+        public StringBuilder Extrato = new StringBuilder();
+        public string NumeroConta { get; set; }
+
+        public string DigitoVerificador { get; set; }
+
+        public double Saldo { get; set; }
+
+        public double Limite { get; set; }
+
+        public double ValorDeposito { get; set; }
+
+        public double ValorSaque { get; set; }
+
+        public double Sacar()
+        {
+
+            if (Saldo < ValorSaque || ValorSaque > Limite)
+            {
+                ValorSaque = 0.00;
+                Console.WriteLine("Saldo Insuficiente");
+                Extrato.AppendLine($"Saldo: {Saldo - ValorSaque}");
+            }
+            else
+            {
+                Extrato.AppendLine($"Saque: {ValorSaque}");
+                Extrato.AppendLine($"Saldo: {Saldo - ValorSaque}");
+                return this.Saldo -= ValorSaque;
+                
+            }
+
+            return Saldo;
+        }
+
+        public double depositar()
+        {
+            Extrato.AppendLine($"Deposito: {ValorDeposito}");
+            Extrato.AppendLine($"Saldo: {Saldo + ValorDeposito}");
+            return this.Saldo += ValorDeposito;
+        }
+
+        
+    }
+}
