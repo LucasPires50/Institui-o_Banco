2bf1bf10fd286263664d4421613b4e7bb2f60adf
Lucas Pires <luc_50@outlook.com>
2019-09-19 13:07:34 -0300

    2º commit: Adicionado os atributos nas classes agencia e cliente

--- Avaliacao_Banco/Classe/Agencia.cs
+++ Avaliacao_Banco/Classe/Agencia.cs
@@ -8,5 +8,22 @@ namespace Avaliacao_Banco.Classe
 {
     class Agencia
     {
+        public string NumeroAgencia { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder InfoAgencia = new StringBuilder();
+            InfoAgencia.AppendLine($"Número Agência: {NumeroAgencia}");
+            InfoAgencia.AppendLine($"Endereço: {Endereco}, {Cidade}-{Estado}");
+
+            return base.ToString();
+        }
+
     }
 }
--- Avaliacao_Banco/Classe/Cliente.cs
+++ Avaliacao_Banco/Classe/Cliente.cs
@@ -8,5 +8,26 @@ namespace Avaliacao_Banco.Classe
 {
     class Cliente
     {
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+        
+        public DateTime DataDeNascimento { get; set;}
+
+        public DateTime DataDeAberturaDaConta { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder InfoCliente = new StringBuilder();
+            InfoCliente.AppendLine($"Nome: {Nome}");
+            InfoCliente.AppendLine($"CPF: {CPF}");
+            InfoCliente.AppendLine($"Data de Nascimento: {DataDeNascimento}");
+            InfoCliente.AppendLine($"Data de abertura da conta: {DataDeAberturaDaConta}");
+
+            return base.ToString();
+        }
+
+
     }
 }
